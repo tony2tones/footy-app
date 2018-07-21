4d595051c7c74cb4160c7bcfbac60f75d010deef
Anthony <anthonymarques@outlook.com>
2018-07-21 16:09:22 +0200

    fix basic app, need to make some API calls stat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-const App = () => {
-  return (
-    <div>
-      <p>React here!</p>
-    </div>
-  );
-};
+
+class App extends Component {
+    constructor() {
+        super(); {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>React here!</p>
+            </div >
+        );
+    }
+}
 export default App;
 ReactDOM.render(<App />, document.getElementById("app"));
