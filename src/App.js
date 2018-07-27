d6d366a184549d576cf9565bd2c7f1ead76305e7
tony2tones <anthony.marque@gmail.com>
2018-07-27 13:49:48 +0200

    working on first time api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+
+// Constant variables
+const apiKEY = 'a04860c9a603472bf0254b397f68fa5db177a1cd6b00e11707023603a957d89f';
+const baseUrl = 'https://apifootball.com/api/?action=';
+
 class App extends Component {
+    static apiForeCast(latitude, longitude) {
+        return `${apiForeCast}?lat=${latitude}&lon=${longitude}&appid=${apiKEY}`;
+    }
     constructor() {
         super(); {
 
         }
+    }
+    componentDidMount() {
+        // https://apifootball.com/api/?action=get_H2H&firstTeam=Chelsea&secondTeam=Arsenal&APIkey=
     }
 
     render() {
