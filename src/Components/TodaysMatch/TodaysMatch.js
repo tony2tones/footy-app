66dbf616d6f5032de7378770e1c1f8bea38d8c3e
tony2tones <anthony.marque@gmail.com>
2018-09-18 08:40:04 +0200

    added Today's match to solution

---
+++ src/Components/TodaysMatch/TodaysMatch.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TodaysMatch extends Component {
+    render() {
+        return (
+            <div>
+                <p>todays game</p>
+            </div>
+        )
+    }
+}
+
+export default TodaysMatch;
