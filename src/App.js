a64ec93e64c5c10ea9eeacc63987ea07b087b0ab
tony2tones <anthony.marque@gmail.com>
2018-09-18 08:31:05 +0200

    added react router dom package

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import request from "superagent";
-import { BrowserRouter } from 'react-router-dom';
+// import request from "superagent";
+import { BrowserRouter } from "react-router-dom";
 
-import FootyApp from './FootyApp/footyApp';
+// import History from './History/History';
+// import TodaysMatch from './TodaysMatch/TodaysMatch';
+import Blog from './Blog/Blog';
 
-import './App.css';
+// import './App.css';
+
+// // Constant variables
+// const apiKEY = 'a04860c9a603472bf0254b397f68fa5db177a1cd6b00e11707023603a957d89f';
+// const baseUrl = 'https://apifootball.com/api/?action=';
+// const resutsUrl = 'https://apifootball.com/api/?action=get_events&from=2016-10-30&to=2016-11-01';
 
 class App extends Component {
-
+    
     render() {
         return (
-            <BrowserRouter>
-                <div className="Blog">
-
-                    <FootyApp />
-                </div >
-            </BrowserRouter>
+            <div>
+                <BrowserRouter>
+                    <Blog />
+                </BrowserRouter>
+            </div>
         );
     }
 }
