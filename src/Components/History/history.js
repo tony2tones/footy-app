c831560c7dcdb68028e14150a8c2619b62499fd4
tony2tones <anthony.marque@gmail.com>
2018-09-18 11:23:07 +0200

    sorting out history logic

--- src/Components/History/history.js
+++ src/Components/History/history.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 // import HomeTeamInput from './HomeTeamInput/HomeTeamInput';
 // import AwayTeamInput from './AwayTeamInput/AwayTeamInput';
@@ -6,7 +7,25 @@ import React, { Component } from "react";
 // import HomeTeam from './HomeTeam/HomeTeam';
 // import Date from './Date/Date';
 
+// Constant variables
+const apiKEY = 'a04860c9a603472bf0254b397f68fa5db177a1cd6b00e11707023603a957d89f';
+const baseUrl = 'https://apifootball.com/api/?action=';
+
 class History extends Component {
+    static headToHead(teamA, teamB) {
+        return `${baseUrl}get_H2H&firstTeam=${teamA}&firstTeam=${teamB}&APIkey=${apiKEY}`;
+    }
+    constructor(props) {
+        super(props); {
+            this.state = {
+                isLoading: true,
+                showResults: false,
+                teamA: 'Chelsea',
+                teamB: 'Liverpool',
+                response: '',
+            }
+        }
+    }
     render() {
         return (
             <div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-require("babel-polyfill");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
