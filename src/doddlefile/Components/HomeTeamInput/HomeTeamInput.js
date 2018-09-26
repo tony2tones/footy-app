0e77da901feb55c972626589153c5b542dc96b91
tony2tones <anthony.marque@gmail.com>
2018-09-26 13:41:21 +0200

    removing un-needed code

--- src/doddlefile/Components/HomeTeamInput/HomeTeamInput.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const HomeTeamInput = (props) => {
-    return <input type="text" onChange={props.changed} value={props.homeTeam}/>;
-};
-
-export default HomeTeamInput;
