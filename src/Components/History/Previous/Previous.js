1a954c63a338e351bc535d2de051371233b22a32
tony2tones <anthony.marque@gmail.com>
2018-09-26 15:18:26 +0200

    added css for Previous component

--- src/Components/History/Previous/Previous.js
+++ src/Components/History/Previous/Previous.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
+import './Previous.css';
 
 const Previous = (props) => {
     var results = props.teamNameList.firstTeam_VS_secondTeam || [];
     // resluts = resluts.slice(0, 1);
     const matchDetailsList = results.map((matchDetails, index) => {
         return (
-            <div key={index}>
+            <div key={index} className="boarder">
                 <div>{matchDetails.match_hometeam_name}</div>
                 <div>{matchDetails.match_hometeam_score}</div>
-                <td>{matchDetails.match_awayteam_name}</td>
-                <div className="nameStyling">{matchDetails.match_awayteam_name}</div>
+                <div>{matchDetails.match_awayteam_name}</div>
                 <div>{matchDetails.match_awayteam_score}</div>
             </div>
         )
@@ -18,7 +18,7 @@ const Previous = (props) => {
 
     return (
         <div>
-            <div className="boarder">
+            <div>
                 {matchDetailsList}
             </div>
         </div >
