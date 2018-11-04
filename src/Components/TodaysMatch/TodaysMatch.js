3b4360ce4f6eb2a625a2f65ec472deb11d3dd6b4
Anthony <anthonymarques@outlook.com>
2018-11-04 13:25:35 +0200

    added date function to put date in correct format

--- src/Components/TodaysMatch/TodaysMatch.js
+++ src/Components/TodaysMatch/TodaysMatch.js
@@ -1,54 +1,65 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-import Results from './Results/Results';
+import Results from "./Results/Results";
 
 // Constant variables
-const apiKEY = 'a04860c9a603472bf0254b397f68fa5db177a1cd6b00e11707023603a957d89f';
-const baseUrl = 'https://apifootball.com/api/?action=';
+const apiKEY =
+  "a04860c9a603472bf0254b397f68fa5db177a1cd6b00e11707023603a957d89f";
+const baseUrl = "https://apifootball.com/api/?action=";
 
 class TodaysMatch extends Component {
-
-    constructor(props) {
-        super(props); {
-            this.state = {
-                dateFrom: '',
-                dateTo: '',
-                response: '',
-            }
-        }
+  constructor(props) {
+    super(props);
+    {
+      this.state = {
+        dateFrom: "",
+        dateTo: "",
+        response: ""
+      };
     }
+  }
 
-    componentDidMount() {
-        this.todaysMatch();
-    }
+  componentDidMount() {
+    this.getTodaysDate();
+    this.todaysMatch();
+  }
 
-    todaysMatch() {
-        axios.get(`${baseUrl}get_events&from=2018-09-22&to=2018-09-22&league_id=62&APIkey=${apiKEY}`)
-            .then((res) => {
-                console.log(res.data);
-                this.setState({
-                    response: res.data,
-                    // isLoading: false,
-                    // showResults: true
-                });
-                // console.log(res.data[0].match_awayteam_name);
-            })
-            .catch(() => {
-                console.log('it didnt work');
-                // will handel this later
-            });
-    };
+  getTodaysDate() {
+    var date = new Date(),
+      month = "-" + (date.getMonth() + 1),
+      day = "-" + date.getDate(),
+      year = + date.getFullYear();
+     console.log(year + month + day);
+  }
 
-    render() {
-        return (
-            <div>
-                <Results 
-                    teamNameList={this.state.response}
-                />
-            </div>
-        )
-    }
+  todaysMatch() {
+    axios
+      .get(
+        `${baseUrl}get_events&from=2018-11-4&to=2018-11-4&league_id=62&APIkey=${apiKEY}`
+      )
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          response: res.data
+          // isLoading: false,
+          // showResults: true
+        });
+        // console.log(res.data[0].match_awayteam_name);
+      })
+      .catch(() => {
+        console.log("it didnt work");
+        // will handel this later
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Results teamNameList={this.state.response} />
+      </div>
+    );
+  }
 }
 
 export default TodaysMatch;
